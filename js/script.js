091963e2bc209d8ec5e950ddfabe0df860ee10bf
Yanfeng Jin <yanfengjin@ipsec-143-215-27-28.vpn.gatech.edu>
2017-07-21 21:31:11 +0800

    Finalize font design for portfolio pages

--- js/script.js
+++ js/script.js
@@ -1,50 +1,4 @@
 $(document).ready(function(){
-
-  // Set navbar content depending on page.
-  let path = window.location.pathname;
-  let page = path.split("/").pop();
-  let portfolioSelected = "";
-  let resumeSelected = "";
-  let aboutSelected = "";
-  switch (page.split(".")[0]) {
-    case "Portfolio":
-      portfolioSelected = "class=\"selected\" ";
-      break;
-
-    case "Resume":
-      resumeSelected = "class=\"selected\" "
-      break;
-
-    case "About":
-      aboutSelected = "class=\"selected\" "
-      break;
-
-    default:
-    //By default, when users are viewing project pages, they should be in the portfolio tab
-      portfolioSelected = "class=\"selected\" ";
-  }
-
-  let navMenus = document.getElementsByClassName('nav-menu-class');
-  for (var i=0;i<navMenus.length;i++) {
-    navMenus[i].innerHTML =
-        '<div class="navbar-header">'+
-          '<button type="button" class="navbar-toggle mobile-opaque" data-toggle="collapse" data-target="#collapsible-nav">'+
-            '<span class="sr-only">切换导航</span>'+
-            '<span class="icon-bar"></span>'+
-            '<span class="icon-bar"></span>'+
-            '<span class="icon-bar"></span>'+
-          '</button>'+
-          '<a class="navbar-brand" href="Portfolio.html">Yanfeng Jin (Tony)</a>'+
-        '</div>'+
-        '<div class="collapse navbar-collapse navbar-right" id="collapsible-nav">'+
-          '<ul class="nav navbar-nav">'+
-            '<li><a '+ portfolioSelected + 'href="Portfolio.html">Portfolio</a></li>'+
-            '<li><a '+ resumeSelected + 'href="#">Resume</a></li>'+
-            '<li><a '+ aboutSelected + 'href="About.html">About</a></li>'+
-          '</ul>'+
-        '</div>';
-  }
-
   $('body').scrollspy({
     target: '.bs-docs-sidebar',
     offset: 40
@@ -68,7 +22,7 @@ $(document).ready(function(){
 	    var $target = $(target);
 
 	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top-50
+	        'scrollTop': $target.offset().top
 	    }, 700, 'swing', function () {
 	        window.location.hash = target;
 	    });
